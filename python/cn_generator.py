7310d278303879e20bcdb456efd60d8c02d61f29
Leonardo Maccari <leonardo.maccari@unitn.it>
2018-10-28 18:15:56 +0100

    now folium supports map and also animations (with some bugs)

--- python/cn_generator.py
+++ python/cn_generator.py
@@ -9,6 +9,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import argparse
 import folium
+from folium import plugins
 
 class CN_Generator():
 
@@ -26,11 +27,14 @@ class CN_Generator():
             self.t = terrain(self.DSN, dataset, ple=2.4)
         else:
             self.t = terrain(DSN, dataset, ple=2.4)
+        self.event_counter = 0
 
     def _post_init(self):
         gateway = self.get_gateway()
         self.infected.append(gateway)
-        self.graph.add_node(gateway.gid, pos=gateway.xy())
+        self.graph.add_node(gateway.gid, pos=gateway.xy(), 
+                            event=self.event_counter)
+        self.event_counter += 1
         self.get_susceptibles()
         print("The gateway is " + repr(gateway))
 
@@ -49,12 +53,83 @@ class CN_Generator():
     def add_links(self, new_node):
         raise NotImplementedError
 
+    def add_node(self, node, pos):
+        self.graph.add_node(node, pos=pos, event=self.event_counter)
+        self.event_counter += 1
+
+    def add_edge(self, src, dst, weight):
+        self.graph.add_edge(src, dst, weight=weight, event=self.event_counter)
+        self.event_counter += 1
+
     def save_graph(self):
         for node in self.graph:
             self.graph.node[node]['x'] = self.graph.node[node]['pos'][0]
             self.graph.node[node]['y'] = self.graph.node[node]['pos'][1]
             del self.graph.node[node]['pos']
         nx.write_graphml(self.graph, self.filename)
+
+    def graph_to_animation(self):
+        location = [(self.t.working_area[1] + self.t.working_area[3])/2,
+                    (self.t.working_area[0] + self.t.working_area[2])/2]
+        box = [
+                [self.t.working_area[1], self.t.working_area[0]],
+                [self.t.working_area[1], self.t.working_area[2]],
+                [self.t.working_area[3], self.t.working_area[2]],
+                [self.t.working_area[3], self.t.working_area[0]],
+                [self.t.working_area[1], self.t.working_area[0]]
+                ]
+        self.m = folium.Map(location=location, zoom_start=13, tiles='Stamen Terrain')
+        edges_s = sorted(self.graph.edges(data=True), key=lambda x: x[2]['event'])
+        nodes_s = sorted(self.graph.nodes(data=True), key=lambda x: x[1]['event'])
+        last_event = max(edges_s[-1][2]['event'], nodes_s[-1][1]['event'])
+        e_coords = []
+        e_times = []
+        for e in edges_s:
+            e_coords.append([list(self.graph.nodes()[e[0]]['pos']),
+                          list(self.graph.nodes()[e[1]]['pos'])])
+            e_times.append(1530744263666+e[2]['event']*36000000)
+            #FIXME starting time is just a random moment
+            
+        features_edges = {
+          'type': 'Feature',
+          'geometry': {
+              'type': 'MultiLineString',
+              'coordinates': e_coords,
+              },
+           'properties': {
+              'times':  e_times,
+              }
+           }
+ 
+        n_coords = []
+        n_times = []
+
+        for n in nodes_s:
+            n_coords.append([n[1]['pos'], n[1]['pos']])
+            n_times.append(1530744263666+n[1]['event']*36000000)
+            
+        # the only way I found to plot the nodes is pretend they are one-point lines
+        features_nodes = {
+          'type': 'Feature',
+          'geometry': {
+              'type': 'MultiLineString',
+              'coordinates': n_coords,
+              },
+           'properties': {
+              'times':  n_times,
+              'style':{
+                  'color': 'red',
+                  'width': 20,
+                  }
+              }
+           }
+
+
+        plugins.TimestampedGeoJson({
+            'type': 'FeatureCollection',
+            'features': [features_edges, features_nodes]},
+            transition_time=500, auto_play=False).add_to(self.m)
+
 
     def graph_to_leaflet(self):
         location = [(self.t.working_area[1] + self.t.working_area[3])/2,
@@ -86,6 +161,10 @@ class CN_Generator():
             map_file = '/tmp/index.html'
             self.m.save(map_file)
             print("A browsable map was saved in " + map_file)
+            self.graph_to_animation()
+            map_file = '/tmp/index-animation.html'
+            self.m.save(map_file)
+            print("A browsable animated map was saved in " + map_file)
 
     def main(self):
         self.display_plot = True
--- python/strategies/growing_network.py
+++ python/strategies/growing_network.py
@@ -67,15 +67,15 @@ class Growing_network(CN_Generator):
         visible_links = self.check_connectivity(new_node)
         # if there's at least one vaild link add the node to the network
         print("testing new node")
+        event = 0
         if visible_links:
             visible_links.sort(key=lambda x: x[2], reverse=True)
             link = visible_links.pop()
             self.infected.append(link[0])
-            self.graph.add_node(link[0].gid, pos=link[0].xy())
-            self.graph.add_edge(link[0].gid, link[1].gid, weight=link[2])
+            self.add_node(link[0].gid, link[0].xy())
+            self.add_edge(link[0].gid, link[1].gid, weight=link[2])
             if len(visible_links) > 1:
                 link = visible_links.pop()
-                self.graph.add_edge(link[0].gid, link[1].gid, weight=link[2])
-                print("added link")
+                self.add_edge(link[0].gid, link[1].gid, weight=link[2])
             return True
         return False
