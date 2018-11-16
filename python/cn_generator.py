2cfe86a76cb7bff52e6e4235905493a0c2b45dd0
Leonardo Maccari <leonardo.maccari@unitn.it>
2018-11-16 12:05:35 +0100

    restructured cn_generator

--- python/cn_generator.py
+++ python/cn_generator.py
@@ -13,12 +13,14 @@ import argparse
 import network
 import folium
 from folium import plugins
+import ubiquiti as ubnt
+from edgeffect import edgeffect
 
 class CN_Generator():
 
     DSN = "postgresql://dbreader@192.168.160.11/terrain_ans"
 
-    def __init__(self, dataset, DSN=None):
+    def __init__(self, dataset, DSN=None, args={}, unk_args={}):
         self.round = 0
         self.infected = []
         self.susceptible = set()
@@ -28,12 +30,23 @@ class CN_Generator():
                                  dest='plot', action='store_true')
         self.parser.add_argument('-b', help="start building latlong (lat.dd,long.dd)", type=str,
                                  required=True)
+        self.parser.add_argument('-n', help="number of nodes", type=int,
+                                 required=True)
+        self.parser.add_argument('-e', help="expansion range (in meters),"
+                                 " defaults to buildings at 30km", type=float,
+                                 default=30000)
+        self.args = self.parser.parse_args(unk_args)
+        self.n = self.args.n
+        self.e = self.args.e
+        self.b = self.args.b
+        self.net.set_maxdev(args.max_dev)
         self.parser.set_defaults(plot=False)
         if not DSN:
             self.t = terrain(self.DSN, dataset, ple=2.4)
         else:
             self.t = terrain(DSN, dataset, ple=2.4)
         self.event_counter = 0
+        ubnt.load_devices()
 
     def _post_init(self):
         latlong = self.b.split(",")
@@ -51,21 +64,91 @@ class CN_Generator():
             raise NoGWError
         return buildings[0]
 
-    def get_newnode(self):
-        raise NotImplementedError
+    def get_random_node(self):
+        new_node = random.sample(self.susceptible, 1)[0]
+        self.susceptible.remove(new_node)
+        return new_node
 
     def get_susceptibles(self):
+        geoms = [g.shape() for g in self.infected]
+        self.sb.set_shape(geoms)
+
+        self.susceptible = set(self.t.get_buildings(
+                               self.sb.get_buffer(self.e))
+                               ) - set(self.infected)
+
+    def get_newnode(self):
         raise NotImplementedError
 
     def stop_condition(self):
         raise NotImplementedError
 
+    def stop_condition_minbw(self):
+        # recompute minimum bw at each node
+        self.net.compute_minimum_bandwidth()
+        # if the minimum bw of a node is less than the treshold stop
+        for n in self.net.graph.nodes():
+            if n == self.net.gateway:
+                continue
+            if self.net.graph.node[n]['min_bw'] < 40:
+                return False
+        return len(self.infected) >= self.n
+
+
+
     def add_links(self, new_node):
         raise NotImplementedError
 
+    def check_link(self, source, destination):
+        phy_link = self.t.get_link(source, destination, h1=2, h2=2)
+        if phy_link and phy_link.loss > 0:
+            link = {}
+            link['src'] = source
+            link['dst'] = destination
+            link['loss'] = phy_link.loss
+            link['src_orient'] = phy_link.Aorient
+            link['dst_orient'] = phy_link.Borient
+            return link
+
+    def check_connectivity(self, set_nodes, new_node):
+        visible_links = []
+        for i in set_nodes:
+            link = self.check_link(source=new_node, destination=i)
+            if link:
+                visible_links.append(link)
+        # with Pool(5) as p:
+        #   TODO: fix client mutlithreading
+        #   self.new_node = new_node
+        #   visible_links = list(set(p.map(self.check_link, self.infected)) - None)
+        return visible_links
+
     def restructure(self):
-        # We dont raise expection because a strategy can implement no restructure
-        pass
+        raise NotImplementedError
+
+    def restructure_edgeeffect(self):
+        # run only every 10 nodes added
+        if self.net.size() % 5 != 0:
+            return
+        # for each link that we found is feasible, but we havent added compute the edge effect
+        for l in self.feasible_links:
+            # we do it only for the link between nodes of the main connected component
+            if not (l['src'].gid in self.net.main_sg() and l['dst'].gid in self.net.main_sg()):
+                l['effect'] = 0
+                continue
+            edge = {}
+            edge[0] = l['src'].gid
+            edge[1] = l['dst'].gid
+            edge['weight'] = 1  # For now do not use costs
+            # TODO: What cost should we use? Can use bandwidth since it depends on the antenna
+            l['effect'] = edgeffect(self.net.main_sg(), edge)
+        # We could just pick up the maximum, but if the link is not negotiable then we should do it again and again
+        # so we order them and we pop them untill the first one connect
+        self.feasible_links.sort(key=lambda x: x['effect'])
+        # Try to connect the best link (try again till something gets connected)
+        while(self.feasible_links):
+            if self.add_link(self.feasible_links.pop()):
+                print("Added one edge")
+                return
     def add_node(self, node):
         self.event_counter += 1
         return self.net.add_node(node, attrs={'event': self.event_counter})
--- python/strategies/growing_network.py
+++ python/strategies/growing_network.py
@@ -5,9 +5,7 @@ from cn_generator import CN_Generator
 from misc import Susceptible_Buffer
 import argparse
 import time
-import ubiquiti as ubnt
 from antenna import Antenna
-from edgeffect import edgeffect
 import code
 
 
@@ -16,69 +14,21 @@ class Growing_network(CN_Generator):
 
     def __init__(self, args, unk_args=None, DSN=None):
         self.sb = Susceptible_Buffer()
-        CN_Generator.__init__(self, args.d, DSN=None)
-        self.parser.add_argument('-n', help="number of nodes", type=int,
-                                 required=True)
-        self.parser.add_argument('-e', help="expansion range (in meters),"
-                                 " defaults to buildings at 30km", type=float,
-                                 default=30000)
-        self.args = self.parser.parse_args(unk_args)
-        self.n = self.args.n
-        self.e = self.args.e
-        self.b = self.args.b
-        self.net.set_maxdev(args.max_dev)
+        CN_Generator.__init__(self, args.d, DSN=None, args=args,
+                              unk_args=unk_args)
         self.feasible_links = []
         self.filename = "graph-%s-%s-%d-%s-%d.graphml"\
                         % (args.d, self.n, int(self.e), self.b, time.time())
         self._post_init()
-        ubnt.load_devices()
-
-    def get_newnode(self):
-        new_node = random.sample(self.susceptible, 1)[0]
-        self.susceptible.remove(new_node)
-        return new_node
-
-    def get_susceptibles(self):
-        geoms = [g.shape() for g in self.infected]
-        self.sb.set_shape(geoms)
-
-        self.susceptible = set(self.t.get_buildings(
-                               self.sb.get_buffer(self.e))
-                               ) - set(self.infected)
 
     def stop_condition(self):
-        # recompute minimum bw at each node
-        self.net.compute_minimum_bandwidth()
-        # if the minimum bw of a node is less than the treshold stop
-        for n in self.net.graph.nodes():
-            if n == self.net.gateway:
-                continue
-            if self.net.graph.node[n]['min_bw'] < 40:
-                return False
-        return len(self.infected) >= self.n
+        return self.stop_condition_minbw()
 
-    def check_link(self, source, destination):
-        phy_link = self.t.get_link(source, destination, h1=2, h2=2)
-        if phy_link and phy_link.loss > 0:
-            link = {}
-            link['src'] = source
-            link['dst'] = destination
-            link['loss'] = phy_link.loss
-            link['src_orient'] = phy_link.Aorient
-            link['dst_orient'] = phy_link.Borient
-            return link
+    def get_newnode(self):
+        return self.get_random_node()
 
-    def check_connectivity(self, set_nodes, new_node):
-        visible_links = []
-        for i in set_nodes:
-            link = self.check_link(source=new_node, destination=i)
-            if link:
-                visible_links.append(link)
-        # with Pool(5) as p:
-        #   TODO: fix client mutlithreading
-        #   self.new_node = new_node
-        #   visible_links = list(set(p.map(self.check_link, self.infected)) - None)
-        return visible_links
+    def restructure(self):
+        return self.restructure_edgeeffect()
 
     def add_links(self, new_node):
         visible_links = self.check_connectivity(self.infected, new_node)
@@ -104,27 +54,3 @@ class Growing_network(CN_Generator):
             return True
         return False
 
-    def restructure(self):
-        # run only every 10 nodes added
-        if self.net.size() % 5 != 0:
-            return
-        # for each link that we found is feasible, but we havent added compute the edge effect
-        for l in self.feasible_links:
-            # we do it only for the link between nodes of the main connected component
-            if not (l['src'].gid in self.net.main_sg() and l['dst'].gid in self.net.main_sg()):
-                l['effect'] = 0
-                continue
-            edge = {}
-            edge[0] = l['src'].gid
-            edge[1] = l['dst'].gid
-            edge['weight'] = 1  # For now do not use costs
-            # TODO: What cost should we use? Can use bandwidth since it depends on the antenna
-            l['effect'] = edgeffect(self.net.main_sg(), edge)
-        # We could just pick up the maximum, but if the link is not negotiable then we should do it again and again
-        # so we order them and we pop them untill the first one connect
-        self.feasible_links.sort(key=lambda x: x['effect'])
-        # Try to connect the best link (try again till something gets connected)
-        while(self.feasible_links):
-            if self.add_link(self.feasible_links.pop()):
-                print("Added one edge")
-                return
