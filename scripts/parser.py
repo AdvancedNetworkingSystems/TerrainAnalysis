2b5de11607be5e7339a1aceb9507c8870e11087a
Gabriele Gemmi <gabriel@autistici.org>
2019-05-22 01:10:37 +0200

    added download in script

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,9 +1,10 @@
 import requests
 import re
 import sys
-from multiprocessing import Pool#, Manager
+from multiprocessing import Pool
 import itertools
 import argparse
+import os
 
 
 class Parser:
@@ -29,29 +30,36 @@ class Parser:
         # self.manager = Manager()
         # self.Global = manager.Namespace()
 
-    def worker(self, xy):
+    def scrape_worker(self, xy):
         url = "http://www502.regione.toscana.it/wmsraster/com.rt.wms.RTmap/wms?map=wmscartoteca&version=1.3.0\
 &map_resolution=91&map_mnt=cartoteca&SERVICE=WMS&FORMAT=image/png&REQUEST=GetFeatureInfo\
 &LAYERS=rt_cartoteca.lidar2k&QUERY_LAYERS=rt_cartoteca.lidar2k&STYLES=solo_contorno_con_etichette\
 &FEATURE_COUNT=50&HEIGHT={0}&WIDTH={1}&SRS=EPSG:25832&VERSION=1.1.1&TRANSPARENT=TRUE\
 &X={2}&Y={3}&BBOX={4}&INFO_FORMAT=text/html".format(self.ymax, self.xmax, xy[0], xy[1], self.bbox)
         r = requests.get(url)
-        #print(r.text)
         match = re.findall('(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])>{}?'.format(self.content), r.text)
         if match:
             url = "{}://{}{}".format(*match[0])
             print(url, file=sys.stderr)
             return url
 
-    def main(self):
+    def scrape(self):
         with Pool(self.processes) as p:
             tuple_space = itertools.product(range(0, self.xmax, 10), range(0, self.ymax, 10))
-            urls = p.map(self.worker, tuple_space)
+            urls = p.map(self.scrape_worker, tuple_space)
         with open("%s_%s.urls" % (self.dataset.lower(), self.content.lower()), "w") as fw:
             urls = set(urls)
-            for u in urls:
-                if u:
-                    print(u, file=fw)
+
+    def download_worker(self, url):
+        os.system("wget -q -P %s_%s %s" % (self.dataset.lower(), self.content.lower(), url[:-1]))
+
+
+    def download(self):
+        with open("%s_%s.url" % (self.dataset.lower(), self.content.lower()), "r") as fr:
+            urls = fr.readlines()
+            os.system("mkdir %s_%s" % (self.dataset.lower(), self.content.lower()))
+            with Pool(self.processes) as p:
+                p.map(self.download_worker, urls)
 
 
 if __name__ == '__main__':
@@ -64,6 +72,11 @@ if __name__ == '__main__':
     parser.add_argument('-c', "--content", help="DSM or DTM",
                              type=str, required=True)
     parser.add_argument('-d', "--dataset", help="Dataset between: Firenze, Pontremoli, Vaiano or Quarrata")
+    parser.add_argument('-sc', action='store_true')
+    parser.add_argument('-dw', action='store_true')
     args = parser.parse_args()
     p = Parser(args.content, args.dataset, args.processes)
-    p.main()
+    if(args.sc):
+        p.scrape()
+    if(args.dw):
+        p.download()
